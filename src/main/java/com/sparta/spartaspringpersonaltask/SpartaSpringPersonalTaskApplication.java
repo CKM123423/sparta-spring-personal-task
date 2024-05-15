d503605d888a8d3f1ba47c4c6eaa889868bc2698
Cho Koung Min <andy123423@naver.com>
2024-05-15 20:10:52 +0900

    init commit

---
+++ src/main/java/com/sparta/spartaspringpersonaltask/SpartaSpringPersonalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaspringpersonaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaSpringPersonalTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaSpringPersonalTaskApplication.class, args);
+    }
+
+}
