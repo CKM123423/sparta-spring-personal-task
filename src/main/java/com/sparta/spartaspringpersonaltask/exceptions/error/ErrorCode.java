58a120e86c98e23cd9683e8f98c044e63f1e0b45
Cho Koung Min <andy123423@naver.com>
2024-05-16 22:40:43 +0900

    feat: ErrorCode 에러관리 이넘 클래스 생성
    Enum 클래스로 에러관리

---
+++ src/main/java/com/sparta/spartaspringpersonaltask/exceptions/error/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.sparta.spartaspringpersonaltask.exceptions.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode{
+
+    // 400번대
+    ALREADY_DELETED_EXCEPTION(HttpStatus.BAD_REQUEST, "이미 삭제된 일정입니다."),
+    INVALID_PASSWORD_EXCEPTION(HttpStatus.UNAUTHORIZED, "비밀번호가 맞지 않습니다."),
+    NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "선택한 일정을 찾지 못하였습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
