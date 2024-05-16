b578c3aa4b9342e287989842dd8e0fc01d0ffdcb
Cho Koung Min <andy123423@naver.com>
2024-05-16 16:41:05 +0900

    feat: ScheduleResponseDto 생성

---
+++ src/main/java/com/sparta/spartaspringpersonaltask/dto/ScheduleResponseDto.java
@@ -0,0 +1,23 @@
+package com.sparta.spartaspringpersonaltask.dto;
+
+import com.sparta.spartaspringpersonaltask.entity.Schedule;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ScheduleResponseDto {
+    private Long scheduleKey;
+    private String scheduleTitle;
+    private String scheduleContent;
+    private String scheduleManager;
+    private LocalDateTime scheduleDatetime;
+
+    public ScheduleResponseDto(Schedule schedule) {
+        this.scheduleKey = schedule.getScheduleKey();
+        this.scheduleTitle = schedule.getScheduleTitle();
+        this.scheduleContent = schedule.getScheduleContent();
+        this.scheduleManager = schedule.getScheduleManager();
+        this.scheduleDatetime = schedule.getScheduleDatetime();
+    }
+}
