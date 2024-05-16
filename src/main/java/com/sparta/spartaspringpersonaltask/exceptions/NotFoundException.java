1180111dc5db07264ccdbdb70a88b7b5fe2117c1
Cho Koung Min <andy123423@naver.com>
2024-05-16 21:26:26 +0900

    feat: NotFoundException 생성

---
+++ src/main/java/com/sparta/spartaspringpersonaltask/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.sparta.spartaspringpersonaltask.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
